897fd21fed70f00e26c6a809b025d0c1b3dcc44f
Alex <alexdekruijff1@gmail.com>
2021-11-29 21:04:23 +0100

    Add files via upload
    cvf

---
+++ client/build/precache-manifest.9fbbe2bcd2d42529bc6e4ef1170dd291.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "421e8fe03d1ca6349cf29da220ae10aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ceb4d9551706cbb8ecb",
+    "url": "/static/css/main.95580698.chunk.css"
+  },
+  {
+    "revision": "7c83a0e4808dc748d3a5",
+    "url": "/static/js/2.b09b8095.chunk.js"
+  },
+  {
+    "revision": "ec266338327c96f8d208bb8386b5b9a5",
+    "url": "/static/js/2.b09b8095.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0275df0ddf00c381912e",
+    "url": "/static/js/3.bb46579e.chunk.js"
+  },
+  {
+    "revision": "6ceb4d9551706cbb8ecb",
+    "url": "/static/js/main.ffef262f.chunk.js"
+  },
+  {
+    "revision": "f570b4aae6154227db72",
+    "url": "/static/js/runtime-main.cff72015.js"
+  },
+  {
+    "revision": "c3addd1b06bbb11b5409d8435c8b12dc",
+    "url": "/static/media/Block.c3addd1b.png"
+  },
+  {
+    "revision": "4c8c8f4c07534c959d96c8c5adc0c3b0",
+    "url": "/static/media/Hash_Function.4c8c8f4c.png"
+  },
+  {
+    "revision": "4ee09de2146fdb54d07b0909eb1c03c9",
+    "url": "/static/media/PoW.4ee09de2.png"
+  }
+]);
